66af39658e583c9b10891b5a75c4c3b77f73d1b9
YetronLives <cnnorteyjr@gmail.com>
2025-12-14 23:09:15 -0500

    test

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,4 +2,8 @@ import NextAuth from "../../../../auth";
 
 export const GET = NextAuth;
 export const POST = NextAuth;
+import NextAuth from "../../../../auth";
+
+export const GET = NextAuth;
+export const POST = NextAuth;
 
