1d182d969ec59393f325599d8a15dc10f61a353c
YetronLives <cnnorteyjr@gmail.com>
2025-12-14 22:37:59 -0500

    ref addition

---
+++ student-link/src/types/next-auth.d.ts
@@ -0,0 +1,35 @@
+import { DefaultSession, DefaultUser } from "next-auth"
+import { DefaultJWT } from "next-auth/jwt"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id?: string
+      username?: string
+      school?: string
+      major?: string
+      year?: string
+      avatarUrl?: string
+    } & DefaultSession["user"]
+  }
+
+  interface User extends DefaultUser {
+    id?: string
+    username?: string
+    school?: string
+    major?: string
+    year?: string
+    avatarUrl?: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    id?: string
+    username?: string
+    school?: string
+    major?: string
+    year?: string
+    avatarUrl?: string
+  }
+}
